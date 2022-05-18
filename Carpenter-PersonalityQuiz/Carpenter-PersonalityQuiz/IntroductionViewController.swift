3bc84b2cd4d82a755fc88e92acb4aa1922ef3b5f
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-05-18 15:14:09 -0400

    Add files via upload

---
+++ Carpenter-PersonalityQuiz/Carpenter-PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Carpenter-PersonalityQuiz
+//
+//  Created by Zachary Carpenter on 5/5/21.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+
+}
+
---
+++ Carpenter-PersonalityQuiz/Carpenter-PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ResultsViewController.swift
+//  Carpenter-PersonalityQuiz
+//
+//  Created by Zachary Carpenter on 5/5/21.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet var resultAnswerLabel: UILabel!
+    @IBOutlet var resultDefinitionLabel: UILabel!
+    
+    
+    var responses: [Answer]
+    
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
+    }
+    
+    func calculatePersonalityResult() {
+        let frequencyOfAnswers = responses.reduce(into:
+            [AnimalType: Int]()) { (counts, answer) in
+            if let existingCount = counts[answer.type] {
+                counts[answer.type] = existingCount + 1
+            } else {
+                counts[answer.type] = 1
+            }
+        }
+        
+        let frequentAnswersSorted = frequencyOfAnswers.sorted(by:
+        { (pair1, pair2) in
+            return pair1.value > pair2.value
+        })
+        
+        let mostCommonAnswer = frequentAnswersSorted.sorted { $0.1 > $1.1 }.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+    }
+    
+}
