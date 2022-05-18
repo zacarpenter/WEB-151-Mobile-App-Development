e3468cb04215830440af4d63aedc9acdc87c701d
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-05-18 15:12:04 -0400

    Add files via upload

---
+++ Carpenter-OrderOfEvents/Carpenter-OrderOfEvents/MiddleViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MiddleViewController.swift
+//  Carpenter-OrderOfEvents
+//
+//  Created by Zachary Carpenter on 4/30/21.
+//
+
+import UIKit
+
+class MiddleViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    var eventNumber : Int = 1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addEvent(from: "viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        addEvent(from: "viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        addEvent(from: "viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        addEvent(from: "viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        addEvent(from: "viewDidDisappear")
+    }
+    
+    func addEvent(from: String) {
+        if let existingText = label.text {
+            label.text = "\(existingText)\nEvent number \(eventNumber) was \(from)"
+            eventNumber += 1
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
